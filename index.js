80c3b6efc368f9398f0a3156fbec3781687e418c
henry_ninor <henry_ninor@cun.edu.co>
2025-06-04 14:48:15 -0500

    frontend in backend

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
